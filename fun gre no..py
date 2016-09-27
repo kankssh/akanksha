85d811334e1c614e8aae973af696f1ebf4ffe1e8
kankssh <yvikas8980@gmail.com>
2016-09-26 23:46:02 -0700

    Add files via upload

---
+++ fun gre no..py
@@ -0,0 +1,8 @@
+n1= int(input("enter the number"))
+n2=int(input("enter the number"))
+def greatest_(n1,n2):
+    if(n1>n2):
+        print("n1 is greatest")
+    else:
+        print("n2 is greatest")
+result= greatest_(n1,n2)
