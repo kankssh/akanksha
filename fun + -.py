13d56d26e4b581e78cade87aa249b08b06617903
kankssh <yvikas8980@gmail.com>
2016-09-26 23:48:17 -0700

    Add files via upload

---
+++ fun + -.py
@@ -0,0 +1,7 @@
+n= int(input("enter the number"))
+def positive_negative(n):
+    if(n>0):
+        print("number is positive")
+    else:
+        print("number is negative")
+result= positive_negative(n)
